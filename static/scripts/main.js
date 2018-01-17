8ad8f8014bd2f185066c5e3a9dad90bf6df28fcf
Mouse Reeve <mousereeve@riseup.net>
2018-01-17 13:11:49 -0500

    Sprucing things up around here

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,16 +1,78 @@
 $(document).ready(function() {
-    $('.option-set').each(function(index) {
-        var options = $(this).children();
-        var selected = options[Math.floor(Math.random()*options.length-1)];
-        $(selected).addClass('visible');
-    });
+    title_toggle($('.title-set'), 0);
 });
 
+var title_toggle = function (optionset, i) {
+    var options = optionset[i];
+    window.setTimeout(function () {
+        toggle(options, function() {
+            if (i + 1 < optionset.length) {
+                title_toggle(optionset, i+1);
+            }
+        });
+    }, 200);
+}
+
 $('.option-set').click(function() {
-    var next = $(this).children('.visible').next();
-    if (!next.length) {
-        next = $(this).children()[0];
-    }
-    $(this).children().removeClass('visible');
-    $(next).addClass('visible');
+    toggle(this);
 });
+
+var toggle = function(options, callback) {
+    var current = $(options).children('.visible');
+    var next = $(options).children('.visible').next();
+    if (!next.length) {
+        next = $(options).children()[0];
+    }
+
+    typeback(current, function() {
+        $(options).children().removeClass('visible');
+
+        var text = $(next).text();
+        $(next).text('');
+        $(next).addClass('visible');
+        typein(next, callback, text, 100);
+    }, 100);
+}
+
+var typeback = function(node, callback, delay) {
+    var text = $(node).html();
+    if (delay === undefined) {
+        $(node).attr('data-text', text);
+        delay = 50;
+    }
+    if (!!text.length) {
+        $(node).html(text.slice(0, -1));
+        window.setTimeout(function () {
+            delay = delay * 0.9;
+            typeback(node, callback, delay);
+        }, delay);
+    } else {
+        $(node).hide();
+        $(node).html($(node).attr('data-text'));
+        if (!!callback) {
+            callback();
+        }
+    }
+};
+
+var typein = function(node, callback, text, delay) {
+    delay = delay || 50;
+    if (text === undefined) {
+        text = $(node).html();
+        $(node).attr('data-text', text);
+        $(node).html('');
+        $(node).show();
+    }
+    if (!!text.length) {
+        $(node).html($(node).html() + text.slice(0, 1));
+        window.setTimeout(function () {
+            delay = delay * 0.99;
+            text = text.slice(1);
+            typein(node, callback, text, delay);
+        }, delay);
+    } else {
+        if (!!callback) {
+            callback();
+        }
+    }
+};
