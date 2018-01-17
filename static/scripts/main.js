f06c84b1acef9414688bf1193d0a7c27eb02f007
Mouse Reeve <mousereeve@riseup.net>
2018-01-17 11:56:15 -0500

    Styling tweaks

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,3 +1,11 @@
+$(document).ready(function() {
+    $('.option-set').each(function(index) {
+        var options = $(this).children();
+        var selected = options[Math.floor(Math.random()*options.length-1)];
+        $(selected).addClass('visible');
+    });
+});
+
 $('.option-set').click(function() {
     var next = $(this).children('.visible').next();
     if (!next.length) {
